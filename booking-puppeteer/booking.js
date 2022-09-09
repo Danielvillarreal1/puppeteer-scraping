d4b8622cbf6eeede0af3ce7618b6bd3f473bb1a4
Daniel <danielvillarreal3425@yahoo.com.ar>
2022-09-09 11:59:17 -0300

    puppeteer booking

--- booking-puppeteer/booking.js
+++ booking-puppeteer/booking.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
 //const ExcelJS = require('exceljs');
+const fs = require('fs');
 const randomUseragent = require('random-useragent');   // npm install random-useragent
+const { get } = require('https');
 
 // creo funcion , pongo el useragent
 const initialization = async () => {
@@ -10,9 +12,10 @@ const initialization = async () => {
     await page.setUserAgent(header);  // usa el useragen
     //await page.setViewport({with: 1920, height: 1080});   // hancho alto compu
     await page.goto('https://www.booking.com/searchresults.es.html?label=gen173nr-1FCAEoggI46AdIM1gEaAyIAQGYAQq4ARnIAQzYAQHoAQH4AQuIAgGoAgO4AsuH6ZgGwAIB0gIkMDU1YWVhNWUtYjc1NS00YTVhLWE4NzYtNmRlNGRkMGVmODFj2AIG4AIB&lang=es&sid=6a0d65e8a01cac135af939438b57a90f&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.es.html%3Flabel%3Dgen173nr-1FCAEoggI46AdIM1gEaAyIAQGYAQq4ARnIAQzYAQHoAQH4AQuIAgGoAgO4AsuH6ZgGwAIB0gIkMDU1YWVhNWUtYjc1NS00YTVhLWE4NzYtNmRlNGRkMGVmODFj2AIG4AIB%26sid%3D6a0d65e8a01cac135af939438b57a90f%26sb_price_type%3Dtotal%26%26&ss=C%C3%B3rdoba%2C+Provincia+de+C%C3%B3rdoba%2C+Argentina&is_ski_area=&ssne=R%C3%ADo+de+Janeiro&ssne_untouched=R%C3%ADo+de+Janeiro&checkin_year=2022&checkin_month=9&checkin_monthday=18&checkout_year=2022&checkout_month=9&checkout_monthday=19&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&search_pageview_id=a06d89a6aa4c0094&ac_suggestion_list_length=5&ac_suggestion_theme_list_length=0&ac_position=2&ac_langcode=es&ac_click_type=b&dest_id=-983417&dest_type=city&iata=COR&place_id_lat=-31.416555&place_id_lon=-64.1837&search_pageview_id=a06d89a6aa4c0094&search_selected=true&ss_raw=arge');   // vista este link
-    await page.screenshot({ path: 'example.png'});
+    //await page.screenshot({ path: 'example.png'});
     await page.waitForSelector('.b978843432');  // espera hasta que el selector tenga la clase
 
+    let data = []
     const itemsList = await page.$$('div[data-testid="property-card"]'); // $$ traiga todo // elemento que se repite
     for (const item of itemsList){
         const name_hotel = await item.$('h3 a .fcab3ed991.a23c043802'); // de la lista de item quiero el precio. $ un elemento
@@ -21,7 +24,7 @@ const initialization = async () => {
         const room = await item.$('.df597226dd');
         const qualification = await item.$('.b1e6dd8416.b48795b3df')
         //const punctuation = await item.$('.b5cd09854e.d10a6220b4')
-        const punctuation = await item.$('div[data-testid="review-score"]')
+        //const punctuation = await item.$('div[data-testid="review-score"]')
         //const availability = await item.$('.cb1f9edcd4')
         // const image = await item.$('div.ui-search-result__image a');
 
@@ -33,16 +36,26 @@ const initialization = async () => {
         const getTax = await page.evaluate(tax => tax.innerText, tax);
         const getRoom = await page.evaluate(room => room.innerText, room);
         const getQualification = await page.evaluate(qualification => qualification.innerText, qualification);
-        const getPunctuation = await page.evaluate(punctuation => punctuation.innerText, punctuation);
+        //const getPunctuation = await page.evaluate(punctuation => punctuation.innerText, punctuation);
         //const getAvailability = await page.evaluate(availability => availability.innerText, availability);
         //console.log('---->', getAvailability)
        
         // const getImage = await page.evaluate(image => image.getAttribute('src'), image);
 
-
+        data.push( // c) al array de data le agregue la data que agarramos previamnete(en reColumns los key)
+        {
+            name: getName,
+            price: getPrice,
+            tax: getTax,
+            room : getRoom,
+            qualification : getQualification
+        }
+        )  
         //console.log(`Name: ${getName} -- Price: ${getPrice} -- Link: ${getImage}`);
-        console.log(`Name hotel: ${getName} -- Price: ${getPrice} -- Tax: ${getTax} -- Room: ${getRoom}  -- Qualification: ${getQualification} -- Punctuation: ${getPunctuation}`);
+        // console.log(`Name hotel: ${getName} -- Price: ${getPrice} -- Tax: ${getTax} -- Room: ${getRoom}  -- Qualification: ${getQualification}`);
+        //console.log(`Name hotel:${getName} Price: ${getPrice} Tax: ${getTax} Room: ${getRoom} Qualification: ${getQualification}`);
     }
+    fs.writeFileSync('./booking.json', JSON.stringify(data, null, 2), 'utf8')
     await browser.close();
 
 }
